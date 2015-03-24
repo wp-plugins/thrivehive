205baa19a8a0a523fe4cb839232b95166c497774
thrivehive <thrivehive@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-24 21:33:47 +0000

    updated version and fixed tabbing issue in readme
    git-svn-id: https://plugins.svn.wordpress.org/thrivehive/branches/testing@1120008 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- thrivehive.php
+++ thrivehive.php
@@ -4,7 +4,7 @@
    *Plugin Name: ThriveHive
    *Plugin URI: http://thrivehive.com
    *Description: A plugin to include ThriveHive's tracking code
-   *Version: 1.66
+   *Version: 1.67
    *Author: ThriveHive
    *Author URI: http://thrivehive.com
    */
@@ -1766,4 +1766,3 @@ function genesis_custom_header_style_override() {
 		printf( '<style type="text/css">%s</style>' . "\n", $output );
 
 }
-
